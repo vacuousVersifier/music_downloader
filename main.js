d59aa3bfebc028dd8968627c07b6a55699dd08a8
Ollie Cockrum (Student) <687676@usd259.net>
2023-02-14 14:15:58 -0600

    Added linting

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "page", "preload.js"),
     },
-  })
-  ipcMain.handle('ping', () => 'pong')
-  win.loadFile('index.html')
-}
+  });
+  ipcMain.handle("ping", () => "HONK HONK");
+  win.loadFile("page/index.html");
+};
 
 app.whenReady().then(() => {
+  // for(let i = 0; i < 5; i++) { createWindow() };
   createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
--- page/preload.js
+++ page/preload.js
@@ -1,8 +1,9 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('versions', {
+contextBridge.exposeInMainWorld("versions", {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke("ping"),
   // we can also expose variables, not just functions
-})
+});
