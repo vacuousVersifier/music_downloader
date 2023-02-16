61dbb887968c7e3100f551064b188f2563012f93
Ollie Cockrum (Student) <687676@usd259.net>
2023-02-16 14:10:29 -0600

    Added link parsing, along with basic html framework

--- page/renderer.js
+++ page/renderer.js
@@ -1,11 +1,33 @@
-const information = document.getElementById("info");
-// information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+$(() => {
+  let link_form = $("#link_form");
+  let link_input = $("#link_input");
+  let link;
 
+  let response_text = $("#response");
 
-const func = async () => {
-  const response = await window.versions.ping();
-  information.innerText = `The response is ${response}`;
-};
+  link_form.on("submit", e => {
+    e.preventDefault();
+    link = link_input.val();
+    link_input.val("");
 
-
-func();
+    let response = "";
+    if(/youtube.com\//gi.test(link)) {
+      // This is a youtube link
+      if(/playlist\?list=/gi.test(link)) {
+        // This is a playlist link
+        response = "Playlist link!";
+      } else if(/watch\?v=/gi.test(link)) {
+        // This is a song link
+        response = "Song link!";
+      } else {
+        response = "This isn't a song or playlist link";
+      }
+    } else if(/youtu\.be\//gi.test(link)) {
+      // This is a short song link
+      response = "Short song link!";
+    } else {
+      response = "This isn't a youtube link";
+    }
+    response_text.text(response);
+  });
+});
