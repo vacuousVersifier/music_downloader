d0c24ae6f71be080b1ff76d388f09a8e6f27337d
Ollie Cockrum (Student) <687676@usd259.net>
2023-02-16 08:49:02 -0600

    Added auto-updates, time to make the prerelease!

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 
+require("update-electron-app")();
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
